950eb99e670995477a69e1f4dd79907013e98c87
codetiger <nnweb365@gmail.com>
2021-02-14 00:02:42 +0800

    newifi

--- scripts/BaseScripts.sh
+++ scripts/BaseScripts.sh
@@ -17,6 +17,7 @@ Diy-Part1() {
     Default_File="$GITHUB_WORKSPACE/lede/package/lean/default-settings/files/zzz-default-settings"
     Date=`date "+%Y/%m/%d"`
     Lede_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Default_File})"
+    mkdir -p $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config > /dev/null 2>1
     mv -f Customize/banner $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
     cd $GITHUB_WORKSPACE/lede/package/base-files/files/bin
     sed -i "s/192.168.1.1/10.10.1.1/g" config_generate
@@ -41,15 +42,18 @@ Diy-Part1() {
         git clone https://github.com/jerrykuku/luci-app-vssr.git 
     fi
     if (( $INCLUDE_mt7621_OC1000MHz == 'true' )); then
+        cd $GITHUB_WORKSPACE
         mv -f Customize/102-mt7621-fix-cpu-clk-add-clkdev.patch $GITHUB_WORKSPACE/lede/target/linux/ramips/patches-5.4
     fi
     if (( $INCLUDE_Enable_FirewallPort_53 == 'true' )); then
         sed -i "s?iptables?#iptables?g"  $GITHUB_WORKSPACE/lede/package/lean/default-settings/files/zzz-default-settings> /dev/null 2>&1
     fi
     if [ $Change_Wifi = 'true' ]; then
+        cd $GITHUB_WORKSPACE
         mv -f Customize/wireless $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
     fi
-    mv -f Customize/mwan3 $GITHUB_WORKSPACE/ledepackage/feeds/packages/mwan3/files/etc/config
+    cd $GITHUB_WORKSPACE
+    mv -f Customize/mwan3 $GITHUB_WORKSPACE/lede/package/feeds/packages/mwan3/files/etc/config
     sed -i "s?Openwrt?Openwrt Compiled By $Author?g" $GITHUB_WORKSPACE/lede/package/base-files/files/etc/banner
     sed -i "s?${Lede_Version}?${Lede_Version} Compiled by ${Author} [$Date]?g" $Default_File
     mv -f Customize/dhcp $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
