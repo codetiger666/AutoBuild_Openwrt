3dc35c7010510673137cd750938ecaf01fd0f116
codetiger <nnweb365@gmail.com>
2021-02-13 23:39:33 +0800

    newifi

--- scripts/BaseScripts.sh
+++ scripts/BaseScripts.sh
@@ -17,35 +17,40 @@ Diy-Part1() {
     Default_File="$GITHUB_WORKSPACE/lede/package/lean/default-settings/files/zzz-default-settings"
     Date=`date "+%Y/%m/%d"`
     Lede_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Default_File})"
-    Replace_File Customize/banner $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
+    mv -f Customize/banner $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
     cd $GITHUB_WORKSPACE/lede/package/base-files/files/bin
     sed -i "s/192.168.1.1/10.10.1.1/g" config_generate
     sed -i "s/192.168/10.10/g" config_generate
-    ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
+    mkdir -p $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
+    mkdir -p  $GITHUB_WORKSPACE/lede/package/CodeTiger
+    cd $GITHUB_WORKSPACE/lede/package/CodeTiger
+    svn checkout https://github.com/openwrt/openwrt/trunk/package/kernel/mt76
+    git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06
+    git clone https://github.com/jerrykuku/luci-app-argon-config.git
     if (( $INCLUDE_SSR_Plus == 'true' )); then
-        cd $GITHUB_WORKSPACE/lede/package
-        ExtraPackages git CodeTiger helloworld https://github.com/fw876 master
+        cd $GITHUB_WORKSPACE/lede/package/CodeTiger
+        git clone https://github.com/fw876/helloworld.git -b master
     fi
     if (( $INCLUDE_Passwall == 'true' )); then
-        cd $GITHUB_WORKSPACE/lede/package
-        ExtraPackages git CodeTiger openwrt-passwall https://github.com/xiaorouji main
+        cd $GITHUB_WORKSPACE/lede/package/CodeTiger
+        git clone https://github.com/xiaorouji/openwrt-passwall.git -b main
     fi
     if (( $INCLUDE_VSSR == 'true' )); then
-        cd $GITHUB_WORKSPACE/lede/package
-        ExtraPackages git CodeTiger lua-maxminddb https://github.com/jerrykuku master
-        ExtraPackages git CodeTiger luci-app-vssr https://github.com/jerrykuku master
+        cd $GITHUB_WORKSPACE/lede/package/CodeTiger
+        git clone https://github.com/jerrykuku/lua-maxminddb.git
+        git clone https://github.com/jerrykuku/luci-app-vssr.git 
     fi
     if (( $INCLUDE_mt7621_OC1000MHz == 'true' )); then
-        Replace_File Customize/102-mt7621-fix-cpu-clk-add-clkdev.patch $GITHUB_WORKSPACE/lede/target/linux/ramips/patches-5.4
+        mv -f Customize/102-mt7621-fix-cpu-clk-add-clkdev.patch $GITHUB_WORKSPACE/lede/target/linux/ramips/patches-5.4
     fi
     if (( $INCLUDE_Enable_FirewallPort_53 == 'true' )); then
         sed -i "s?iptables?#iptables?g"  $GITHUB_WORKSPACE/lede/package/lean/default-settings/files/zzz-default-settings> /dev/null 2>&1
     fi
     if [ $Change_Wifi = 'true' ]; then
-        Replace_File Customize/wireless $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
+        mv -f Customize/wireless $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
     fi
-    Replace_File Customize/mwan3 $GITHUB_WORKSPACE/ledepackage/feeds/packages/mwan3/files/etc/config
+    mv -f Customize/mwan3 $GITHUB_WORKSPACE/ledepackage/feeds/packages/mwan3/files/etc/config
     sed -i "s?Openwrt?Openwrt Compiled By $Author?g" $GITHUB_WORKSPACE/lede/package/base-files/files/etc/banner
     sed -i "s?${Lede_Version}?${Lede_Version} Compiled by ${Author} [$Date]?g" $Default_File
-    Replace_File Customize/dhcp $GITHUB_WORKSPACE/ledepackage/feeds/packages/mwan3/files/etc/config
+    mv -f Customize/dhcp $GITHUB_WORKSPACE/lede/package/base-files/files/etc/config
 }
